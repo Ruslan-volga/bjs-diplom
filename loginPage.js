096b65d6607805976a3d200baa4f543e774326c6
Ruslan-Volga <rustikty@gmail.com>
2025-04-01 22:38:00 +0300

    Вывод сообщения обошибке на страницу

--- loginPage.js
+++ loginPage.js
@@ -1,26 +1,23 @@
 const userForm = new UserForm();
 
-
 userForm.loginFormCallback = function(data) {
     ApiConnector.login(data, (response) => {
         if (response.success) {
-            
             location.reload(); 
         } else {
-            
-            console.log(response.error || "Ошибка авторизации");
+            const message = "Ошибка авторизации";
+            this.setLoginErrorMessage(message); 
         }
     });
 };
 
-
 userForm.registerFormCallback = function(data) {
     ApiConnector.register(data, (response) => {
         if (response.success) {
-            
             location.reload(); 
         } else {
-            console.log(response.error || "Ошибка регистрации");
+            const message = "Ошибка регистрации";
+            this.setRegisterErrorMessage(message); 
         }
     });
 };
